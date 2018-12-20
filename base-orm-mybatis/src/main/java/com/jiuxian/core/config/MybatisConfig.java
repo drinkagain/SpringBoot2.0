4f460e69d011f1bf282ccb2dbaab7c04832168d1
熊猫酒仙 <857591294@qq.com>
2018-12-20 14:56:44 +0800

    字段自动 填充

--- base-orm-mybatis/src/main/java/com/jiuxian/core/config/MybatisConfig.java
+++ base-orm-mybatis/src/main/java/com/jiuxian/core/config/MybatisConfig.java
@@ -1,13 +1,17 @@
 package com.jiuxian.core.config;
 
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.apache.ibatis.reflection.MetaObject;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Date;
+
 @Configuration
 @MapperScan("com.jiuxian.**.mapper")
-public class MybatisConfig {
+public class MybatisConfig implements MetaObjectHandler {
 
     /**
      * 分页插件
@@ -17,4 +21,23 @@ public class MybatisConfig {
         return new PaginationInterceptor();
     }
 
+    /**
+     * 公用字段配置
+     */
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        if (metaObject.hasSetter("creatTime")) {
+            metaObject.setValue("creatTime", new Date());
+        }
+        if (metaObject.hasSetter("updateTime")) {
+            metaObject.setValue("updateTime", new Date());
+        }
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        if (metaObject.hasSetter("updateTime")) {
+            metaObject.setValue("updateTime", new Date());
+        }
+    }
 }
