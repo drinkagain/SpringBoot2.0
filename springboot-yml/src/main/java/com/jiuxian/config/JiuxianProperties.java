a49043ab9be2c18d71ecc5a14b2cb3f4c2be6e6c
Zejun-Liu <857591294@qq.com>
2019-01-24 16:58:51 +0800

    配置文件有提示信息

--- springboot-yml/src/main/java/com/jiuxian/config/JiuxianProperties.java
+++ springboot-yml/src/main/java/com/jiuxian/config/JiuxianProperties.java
@@ -1,136 +1,47 @@
 package com.jiuxian.config;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-
-import java.util.Arrays;
+import org.springframework.boot.context.properties.DeprecatedConfigurationProperty;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
 
 /**
  * Author: LIU ZEJUN
  * Date: 2019-01-23 16:04:00
  * Comment:
  */
-
+@Data
 @ConfigurationProperties(prefix = "jiuxian")
 public class JiuxianProperties {
+
     private String name;
     private String nameCn;
     private String nameEn;
     private String[] hobbies;
     private SexEnum sexEnum;
     private boolean single;
+
+    @NestedConfigurationProperty
     private School school;
 
+    private City city;
+
+    @DeprecatedConfigurationProperty(replacement = "jiuxian.name-cn", reason = "deprecated")
     public String getName() {
-        return this.name;
+        return getNameCn();
     }
 
     public void setName(String name) {
-        this.name = name;
-    }
-
-
-    public String getNameCn() {
-        return nameCn;
-    }
-
-    public void setNameCn(String nameCn) {
-        this.nameCn = nameCn;
-    }
-
-    public String getNameEn() {
-        return nameEn;
-    }
-
-    public void setNameEn(String nameEn) {
-        this.nameEn = nameEn;
-    }
-
-    public String[] getHobbies() {
-        return hobbies;
-    }
-
-
-    public void setHobbies(String[] hobbies) {
-        this.hobbies = hobbies;
-    }
-
-    public SexEnum getSexEnum() {
-        return sexEnum;
-    }
-
-    public void setSexEnum(SexEnum sexEnum) {
-        this.sexEnum = sexEnum;
-    }
-
-    public boolean isSingle() {
-        return single;
-    }
-
-    public void setSingle(boolean single) {
-        this.single = single;
-    }
-
-    public School getSchool() {
-        return school;
-    }
-
-    public void setSchool(School school) {
-        this.school = school;
-    }
-
-    static class School {
-        private Integer no;
-        private String name;
-        private String address;
-
-        public Integer getNo() {
-            return no;
-        }
-
-        public void setNo(Integer no) {
-            this.no = no;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getAddress() {
-            return address;
-        }
-
-        public void setAddress(String address) {
-            this.address = address;
-        }
-
-        @Override
-        public String toString() {
-            return "School{" +
-                    "no=" + no +
-                    ", name='" + name + '\'' +
-                    ", address='" + address + '\'' +
-                    '}';
-        }
+        setNameCn(name);
     }
 
     enum SexEnum {
         MAN, WOMAN
     }
 
-    @Override
-    public String toString() {
-        return "JiuxianProperties{" +
-                "name='" + name + '\'' +
-                ", nameCn='" + nameCn + '\'' +
-                ", nameEn='" + nameEn + '\'' +
-                ", hobbies=" + Arrays.toString(hobbies) +
-                ", sexEnum=" + sexEnum +
-                ", single=" + single +
-                ", school=" + school +
-                '}';
+    @Data
+    static class City {
+        private String no;
+        private String name;
     }
 }
---
+++ springboot-yml/src/main/java/com/jiuxian/config/School.java
@@ -0,0 +1,17 @@
+package com.jiuxian.config;
+
+import lombok.Data;
+
+/**
+ * Author: LIU ZEJUN
+ * Date: 2019-01-24 14:03:00
+ * Comment:
+ */
+
+@Data
+public class School {
+
+    private Integer no;
+    private String name;
+    private String address;
+}
